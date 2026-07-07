f525672afc01e3417a86dec19a2891372e89a07b
Hugo Rizzo <rizzo_hugo@hotmail.com>
2026-07-07 12:22:39 -0300

    Updating portfolio

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "../../style/App.css";
 import Navbar from "../../components/navbar/navbar.js";
 import Footer from "../../components/footer/footer.js";
@@ -6,22 +6,40 @@ import Footer from "../../components/footer/footer.js";
 const About = () => {
   return (
     <main>
-    <Navbar />
-    <div className="about-container">
-      <section className="about-header">
-        <h1>ABOUT ME</h1>
-        <p className="about-description">
-        My name is Hugo Rizzo, I'm a Front-End developer.
-        </p>
-        <p className="about-description">
-        I am passionate about technology and Front-End development.
-        </p>
-        <p className="about-description">
-        I am currently a Computer Engineering student in Brazil and work at <a className="RookTechPresentation" href="https://rook-tech.com">Rook Tech</a>.
-        </p>
-      </section>
-    </div>
-    <Footer />
+      <Navbar />
+      <div className="about-container">
+        <section className="about-header">
+          <h1>ABOUT ME</h1>
+          <p className="about-description">
+            My name is Hugo Rizzo, a Front-End developer graduated in Computer
+            Engineering in Brazil.
+          </p>
+          <p className="about-description">
+            My journey in the technology field began with curiosity about web
+            development, which sparked a strong interest and drove me to seek
+            further knowledge regarding new technologies and their applications.
+          </p>
+          <p className="about-description">
+            I enjoy transforming ideas into responsive, user-friendly web
+            experiences, and I am always looking to learn new tools and ways to
+            write cleaner, better code.
+          </p>
+          <p className="about-description">
+            I also have hands-on experience using AI tools to speed up
+            development and to integrate AI capabilities directly into systems,
+            applying them daily to write better code and build smarter
+            solutions.
+          </p>
+          <p className="about-description">
+            Currently, I work as a Fullstack developer at{" "}
+            <a className="RookTechPresentation" href="https://rook-tech.com">
+              Rook Tech
+            </a>
+            , where I build and improve real-world products.
+          </p>
+        </section>
+      </div>
+      <Footer />
     </main>
   );
 };
--- portfolio/src/pages/Projects/Projects.js
+++ portfolio/src/pages/Projects/Projects.js
@@ -11,6 +11,23 @@ import img3Sh from "../../images/ShoppingList/Buy3.png";
 import img1Fk from "../../images/Fokus/Fokus1.png";
 import img2Fk from "../../images/Fokus/Fokus2.png";
 import img3Fk from "../../images/Fokus/Fokus3.png";
+import langoD1 from "../../images/LangoDawg/LangoDawg1.png";
+import langoD2 from "../../images/LangoDawg/LangoDawg2.png";
+import langoD3 from "../../images/LangoDawg/LangoDawg3.png";
+import langoD4 from "../../images/LangoDawg/LangoDawg4.png";
+import langoD5 from "../../images/LangoDawg/LangoDawg5.png";
+import langoD6 from "../../images/LangoDawg/LangoDawg6.png";
+import langoD7 from "../../images/LangoDawg/LangoDawg7.png";
+import langoD8 from "../../images/LangoDawg/LangoDawg8.png";
+import langoD9 from "../../images/LangoDawg/LangoDawg9.png";
+import langoM1 from "../../images/LangoDawgMobile/Image 1.jpg";
+import langoM2 from "../../images/LangoDawgMobile/Image 2.jpg";
+import langoM3 from "../../images/LangoDawgMobile/Image 3.jpg";
+import langoM4 from "../../images/LangoDawgMobile/Image 4.jpg";
+import langoM5 from "../../images/LangoDawgMobile/Image 5.jpg";
+import langoM6 from "../../images/LangoDawgMobile/Image 6.jpg";
+import langoM7 from "../../images/LangoDawgMobile/Image 7.jpg";
+import langoM8 from "../../images/LangoDawgMobile/Image 8.jpg";
 
 const Carousel = ({ images, onImageClick }) => {
     const [currentIndex, setCurrentIndex] = React.useState(0);
@@ -60,16 +77,26 @@ const Projects = () => {
     const works = [
         {
             id: 1,
+            images: [langoD1, langoD2, langoD3, langoD4, langoD5, langoD6, langoD7, langoD8, langoD9],
+            title: <a className='linkProjects' href='https://langodawg.com/'>LangoDawg</a>,
+        },
+        {
+            id: 2,
+            images: [langoM1, langoM2, langoM3, langoM4, langoM5, langoM6, langoM7, langoM8],
+            title: "LangoDawg Mobile",
+        },
+        {
+            id: 3,
             images: [img1S, img2S, img3S],
             title: <a className='linkProjects' href='https://github.com/Hugo-Rizzo-Dev/search'>CEP SEARCH</a>,
         },
         {
-            id: 2,
+            id: 4,
             images: [img2Sh, img1Sh, img3Sh],
             title: <a className='linkProjects' href='https://github.com/Hugo-Rizzo-Dev/listaDeCompras2'>SHOPPING LIST</a>,
         },
         {
-            id: 3,
+            id: 5,
             images: [img1Fk, img2Fk, img3Fk],
             title: <a className='linkProjects' href='https://github.com/Hugo-Rizzo-Dev/Fokus'>FOKUS</a>,
         },
